f73589c9e3f4f5bd0f769f6573fd5d052b133d1b
William Souza <Will051096@gmail.com>
2022-10-12 19:16:52 -0300

    commit inicial

---
+++ lib/app/core/ui/styles/text_styles.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
+
+class TextStyles {
+  static TextStyles? _instance;
+
+  TextStyles._();
+
+  static TextStyles get i {
+    _instance ??= TextStyles._();
+    return _instance!;
+  }
+
+  String get primaryFont => 'Poppins';
+  String get secundaryFont => 'MPlus1P';
+
+  // primary font
+
+  TextStyle get textPrimaryFontRegular =>
+      TextStyle(fontWeight: FontWeight.normal, fontFamily: primaryFont);
+
+  TextStyle get textPrimaryFontMedium =>
+      TextStyle(fontWeight: FontWeight.w500, fontFamily: primaryFont);
+
+  TextStyle get textPrimaryFontSemiBold =>
+      TextStyle(fontWeight: FontWeight.w600, fontFamily: primaryFont);
+
+  TextStyle get textPrimaryFontBold =>
+      TextStyle(fontWeight: FontWeight.bold, fontFamily: primaryFont);
+
+  TextStyle get textPrimaryFontExtraBold =>
+      TextStyle(fontWeight: FontWeight.w800, fontFamily: primaryFont);
+
+  // Secundary Font
+
+  TextStyle get textSecundaryFontRegular =>
+      TextStyle(fontWeight: FontWeight.normal, fontFamily: secundaryFont);
+
+  TextStyle get textSecundaryFontMedium =>
+      TextStyle(fontWeight: FontWeight.w600, fontFamily: secundaryFont);
+
+  TextStyle get textSecundaryFontBold =>
+      TextStyle(fontWeight: FontWeight.bold, fontFamily: secundaryFont);
+
+  TextStyle get textSecundaryFontExtraBold =>
+      TextStyle(fontWeight: FontWeight.w800, fontFamily: secundaryFont);
+
+  TextStyle get labelTextField =>
+      textSecundaryFontRegular.copyWith(color: ColorsApp.i.greyDark);
+
+  TextStyle get textSecundaryFontExtraBoldPrimaryColor =>
+      textSecundaryFontExtraBold.copyWith(color: ColorsApp.i.primary);
+}
+
+extension TextStylesExtensions on BuildContext {
+  TextStyles get textStyles => TextStyles.i;
+}
